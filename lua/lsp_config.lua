34289f90663363b5d049982cc43fb6a2db1c9ae9
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-04-14 10:18:46 +0200

    Use clangd as C++ LSP
    Also move the diagnostics config inside the `if vim.lsp` block.

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -69,23 +69,30 @@ if vim.lsp then
             }
         }
     }
+
+    -- colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+    -- for f in build/*/compile_commands.json; do ln -s $(realpath $f) $(echo $f | sed 's/build/src/'); done
+    lspconfig.clangd.setup{
+        on_attach = on_attach,
+        cmd = { "clangd-9", "--background-index" },
+    }
+
+    -- Settings for displaying LSP diagnostics
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        -- This will disable virtual text, like doing:
+        -- let g:diagnostic_enable_virtual_text = 0
+        virtual_text = false,
+
+        -- This is similar to:
+        -- let g:diagnostic_show_sign = 1
+        -- To configure sign display,
+        --  see: ":help vim.lsp.diagnostic.set_signs()"
+        signs = true,
+
+        -- This is similar to:
+        -- "let g:diagnostic_insert_delay = 1"
+        update_in_insert = false,
+    }
+    )
 end
-
--- Settings for displaying LSP diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- This will disable virtual text, like doing:
-    -- let g:diagnostic_enable_virtual_text = 0
-    virtual_text = false,
-
-    -- This is similar to:
-    -- let g:diagnostic_show_sign = 1
-    -- To configure sign display,
-    --  see: ":help vim.lsp.diagnostic.set_signs()"
-    signs = true,
-
-    -- This is similar to:
-    -- "let g:diagnostic_insert_delay = 1"
-    update_in_insert = false,
-}
-)
