e0ff751ab3997b852df4c71f00b5084883eab160
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-01-13 13:36:38 +0100

    lsp: Use clangd-12

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -87,7 +87,7 @@ if vim.lsp then
     lspconfig.clangd.setup{
         on_attach = on_attach,
         capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-        cmd = { "clangd-10", "--background-index" },
+        cmd = { "clangd-12", "--background-index" },
     }
 
     -- Settings for displaying LSP diagnostics
