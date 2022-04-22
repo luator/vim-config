76c4eac3f06eac84d9c8550b310e4842026300b4
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-04-22 09:10:27 +0200

    treesitter: ensure_installed="maintained" to "all"
    "maintained" is deprecated so switch to "all".

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,7 +1,7 @@
 -- nvim-treesitter config
 require'nvim-treesitter.configs'.setup {
-    -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "maintained",
+    -- one of "all" or a list of languages
+    ensure_installed = "all",
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = true, -- to prevent spell checking of source code
