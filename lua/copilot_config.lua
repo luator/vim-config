d2e1f3df07fc74811bd32c8a742933abbac46e06
Felix Kloss <felix.kloss@uni-tuebingen.de>
2024-12-10 18:01:05 +0100

    Update copilot config

--- lua/copilot_config.lua
+++ lua/copilot_config.lua
@@ -1,6 +1,6 @@
 require('copilot').setup({
   panel = {
-    enabled = true,
+    enabled = false,
     auto_refresh = false,
     keymap = {
       jump_prev = "[[",
@@ -23,9 +23,9 @@ require('copilot').setup({
       accept = "<M-l>",
       accept_word = false,
       accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
+      next = "<M-j>",
+      prev = "<M-k>",
+      dismiss = "<C-l>",
     },
   },
   --filetypes = {
@@ -43,6 +43,7 @@ require('copilot').setup({
       ["*"] = false,  -- disable for all by default
       -- only enable for specific filetypes
       cpp = true,
+      dot = true,
       lua = true,
       python = true,
       rust = true,
@@ -51,3 +52,7 @@ require('copilot').setup({
   copilot_node_command = "/home/felixwidmaier/.local/bin/node_latest.sif",
   server_opts_overrides = {},
 })
+
+require("copilot_cmp").setup()
+
+require("CopilotChat").setup()
