8b6c61daeeb98cfdfdd3a4e9b44753aa4f149319
Felix Kloss <felix.kloss@uni-tuebingen.de>
2025-02-12 14:02:44 +0100

    Use treesitter as provider for ufo

--- lua/folding.lua
+++ lua/folding.lua
@@ -25,15 +25,15 @@ vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 --    })
 --end
 
-require('ufo').setup()
+--require('ufo').setup()
 --
 
 -- Option 3: treesitter as a main provider instead
 -- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
 -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
---require('ufo').setup({
---    provider_selector = function(bufnr, filetype, buftype)
---        return {'treesitter', 'indent'}
---    end
---})
+require('ufo').setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+})
 --
